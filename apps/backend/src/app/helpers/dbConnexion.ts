c75806a11878cf97f0a0eb387775b582a1b7eb6f
akladekouassi <akladekouassi@gmail.com>
2021-05-07 16:25:16 +0000

    update script

--- apps/backend/src/app/helpers/dbConnexion.ts
+++ apps/backend/src/app/helpers/dbConnexion.ts
@@ -1,14 +1,9 @@
 import mongoose = require('mongoose');
 
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGODB_USER_PASS}@${process.env.MONGODB_URL}/${process.env.MONGODB_NAME}?authSource=${process.env.MONGODB_AUTH_SOURCE}&authMechanism=${process.env.MONGODB_AUTH_MECHANISME}`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(`mongodb+srv://Akladekouassi:Akladekouassi@cluster0.8stux.mongodb.net/Botmind?authMechanism=SCRAM-SHA-1`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
   .then(() => console.log('MONGODB connected successfully'))
   .catch(err => console.log('Error occured when connecting to the DB:', err));
